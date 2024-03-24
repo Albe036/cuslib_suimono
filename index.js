b7c63e434f4a594e5faf6c06aec0b7b79729937b
Albeiro Lozano <57449621+Albe036@users.noreply.github.com>
2024-03-24 09:27:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { generate, generate2, saludar } from "./packages/pack1/index.js";
 import { generatepack2 } from "./packages/pack2/index.js";
+import { faker } from '@faker-js/faker';
 
 export { generate, generate2, saludar, generatepack2 }
